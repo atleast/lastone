2db7e517eea3734ad076b4a48168768000df284e
atleast <atleast886@gmail.com>
2015-09-29 18:57:36 +0800

    Single View App demo

--- LastStone/LastStone/ViewController.h
+++ LastStone/LastStone/ViewController.h
@@ -8,17 +8,30 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UIActionSheetDelegate>
 
+//UITextField, UIButtoon, KeyBoard
 @property (weak, nonatomic) IBOutlet UITextField *textField;
 - (IBAction)btnClicked:(id)sender;
 - (IBAction)didInput:(id)sender;
 - (IBAction)tapBg:(id)sender;
 
+//Button
 @property (weak, nonatomic) IBOutlet UILabel *buttonStatus;
 - (IBAction)btnPressed:(id)sender;
 
+//Slider
 @property (weak, nonatomic) IBOutlet UILabel *slideNum;
 - (IBAction)changeSlider:(id)sender;
+
+//Switch, Segments
+@property (weak, nonatomic) IBOutlet UISwitch *leftSwitch;
+@property (weak, nonatomic) IBOutlet UISwitch *rightSwitch;
+@property (weak, nonatomic) IBOutlet UIButton *buttonShowAndHide;
+
+- (IBAction)switchChanged:(id)sender;
+- (IBAction)toggleChanged:(id)sender;
+- (IBAction)shownAndPressed:(id)sender;
+
 
 @end
